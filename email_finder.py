598f3d3eff5a995c2eaa03df908b4cc3f12ddce9
Szymon Radomski <107359025+SzymCode@users.noreply.github.com>
2022-10-29 18:55:13 +0200

    Create email_finder.py

---
+++ email_finder.py
@@ -0,0 +1,28 @@
+import pyperclip, re
+
+
+def email_finder():
+    email = re.compile(r'''(
+        [a-zA-Z/d._%+-]+     
+        @                      
+        [a-zA-Z/d.-]+          
+        (\.[a-zA-Z]{2,4}){1,2} 
+        )''', re.VERBOSE)
+
+
+    text = str(pyperclip.paste())
+    matches = []
+
+    for groups in email.findall(text):
+        matches.append(groups[0])
+
+
+    if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Copied to the clipboard:')
+        print('\n'.join(matches))
+    else:
+        print('No e-mail address was found.')
+
+if __name__ == "__main__":
+    email_finder()
