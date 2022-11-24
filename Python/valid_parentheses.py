222d2102dd0cbddfc508059c4939f0b47ab543c8
Szymon Radomski <s.radomski19@gmail.com>
2022-11-24 17:01:53 +0100

    Create valid_parentheses.py

---
+++ Python/valid_parentheses.py
@@ -0,0 +1,17 @@
+def valid_parentheses(string):
+  
+    mylist = []
+    for s in string:
+        if s == '(':
+            mylist.append(s)
+        elif s == ')':
+            try:
+                mylist.pop()
+            except:
+                return False
+
+    if len(mylist) == 0:
+        return True
+    else:
+        return False
+      
