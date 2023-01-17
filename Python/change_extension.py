ededacfe08c2ab5180dee0b07d5b8a129f9635d0
Szymon Radomski <s.radomski19@gmail.com>
2023-01-17 09:27:42 +0100

    Create change_extension.py

---
+++ Python/change_extension.py
@@ -0,0 +1,12 @@
+import os
+
+def change_extension(path)
+    current_extension = ".txt"
+    new_extension = ".pdf"
+
+    for filename in os.listdir(path):
+        if filename.endswith(current_extension):
+            filepath = os.path.join(path, filename)
+            os.rename(filepath, filepath.replace(current_extension, new_extension))
+
+change_extension(input("Enter full path to change files extensions in directory: "))
