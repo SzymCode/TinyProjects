804ccc26e9532ad4697c24714a4ee31ecdffbd00
Szymon Radomski <s.radomski19@gmail.com>
2023-03-10 22:04:20 +0100

    Create download_manager.py

---
+++ Python/download_manager.py
@@ -0,0 +1,97 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+
+source_dir = "C:\\Users\\Admin\\Downloads"
+dest_dir_sfx = "C:\\Users\\Admin\\Music"
+dest_dir_music = "C:\\Users\\Admin\\Music"
+dest_dir_video = "C:\\Users\\Admin\\Videos"
+dest_dir_image = "C:\\Users\\Admin\\Pictures"
+dest_dir_documents = "C:\\Users\\Admin\\Documents"
+
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd",
+                    ".raw", ".arw", ".cr2", ".nrw", ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt",
+                    ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+document_extensions = [".doc", ".docx", ".odt", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+
+def make_unique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+
+    return name
+
+
+def move_file(dest, entry, name):
+    if exists(f"{dest}/{name}"):
+        unique_name = make_unique(dest, name)
+        old_name = join(dest, name)
+        new_name = join(dest, unique_name)
+        rename(old_name, new_name)
+    move(entry, dest)
+
+
+def check_audio_files(entry, name):
+    for audio_extension in audio_extensions:
+        if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+            if entry.stat().st_size < 10_000_000 or "SFX" in name:
+                dest = dest_dir_sfx
+            else:
+                dest = dest_dir_music
+            move_file(dest, entry, name)
+
+
+def check_video_files(entry, name):
+    for video_extension in video_extensions:
+        if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+            move_file(dest_dir_video, entry, name)
+
+
+def check_image_files(entry, name):
+    for image_extension in image_extensions:
+        if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+            move_file(dest_dir_image, entry, name)
+
+
+def check_document_files(entry, name):
+    for documents_extension in document_extensions:
+        if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+            move_file(dest_dir_documents, entry, name)
+
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                check_audio_files(entry, name)
+                check_video_files(entry, name)
+                check_image_files(entry, name)
+                check_document_files(entry, name)
+
+
+if __name__ == "__main__":
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
